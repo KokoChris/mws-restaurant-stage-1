c2e580ab900fd029cd9873a56c473c0cd05fd98e
Christos <christos@workable.com>
2019-03-16 11:50:22 +0200

    Makes main and details pages responsive.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY2hyaXN0b3N3b3JrYWJsZSIsImEiOiJjanRiNzBtMzMwaHZvNDRxdHZxcm1iMHlyIn0.KT2zhzxC8471k7yre9ek3w',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
