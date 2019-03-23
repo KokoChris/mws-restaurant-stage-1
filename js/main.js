ff3417b98613e82335383d698aabe586eba814af
Christos <christos@workable.com>
2019-03-23 12:17:46 +0200

    Adds basic accesibility enhancements

--- js/main.js
+++ js/main.js
@@ -194,6 +194,7 @@ createRestaurantHTML = restaurant => {
 
   const more = document.createElement("a");
   more.innerHTML = "View Details";
+  more.role = "navigation";
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more);
 
